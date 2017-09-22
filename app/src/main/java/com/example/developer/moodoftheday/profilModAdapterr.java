af178b4aef7c8bf9ff1ba27069b88df9b0312fb7
Rumeysa, Bingöl <rumeysa.bingol@agu.edu.tr>
2017-09-22 17:53:02 +0300

    news

--- app/src/main/java/com/example/developer/moodoftheday/profilModAdapterr.java
+++ app/src/main/java/com/example/developer/moodoftheday/profilModAdapterr.java
@@ -121,7 +121,7 @@ public class profilModAdapterr extends RecyclerView.Adapter<profilModAdapterr.Vi
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         //
-        user=FirebaseAuth.getInstance().getCurrentUser().getUid();
+       // user=FirebaseAuth.getInstance().getCurrentUser().getUid();
         //user=modDurumlarıList.get(position).getId();
         refKisiBil=FirebaseDatabase.getInstance().getReference("users");
       //  refKisiBil.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
